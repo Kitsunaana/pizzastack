9540b6c9a1f4c691b7ddd51997b0a3f8d4f91e3a
Deni Selimkhanov <selimkhanov_deni@list.ru>
2023-02-24 12:25:17 +0700

    Настройка Docker

---
+++ hasura/seeds/default/1677216212856_menuSeed.sql
@@ -0,0 +1,3 @@
+SET check_function_bodies = false;
+INSERT INTO public.menu (id, image, weight, title, ingredients, price) VALUES ('de9b9043-60a4-495d-a053-8269d7ccf0c1', 'manhattan.jpeg', 555, 'Пицца Мангеттен', '(двойная порция грибов), Грибы, Моцарелла, Пеперони, Соус Альфредо', 215);
+INSERT INTO public.menu (id, image, weight, title, ingredients, price) VALUES ('cead2b1e-57f3-4352-90fa-c3a3727b51a3', 'pepperoni-w-tomatoes.jpeg', 554, 'Пицца Пеперони с томатами', 'Моцарелла, Пеперони, Помидоры, Соус Барбекю', 215);
